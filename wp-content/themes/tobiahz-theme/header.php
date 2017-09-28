524ef037e5364c9d010c77963d4a0060676f0947
Tobiah Zarlez <TobiahZ@users.noreply.github.com>
2017-09-28 15:39:51 -0700

    Added TobiahZ theme

---
+++ wp-content/themes/tobiahz-theme/header.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta name="application-name" content="Tobiah Zarlez Blog"/>
+	<meta name="t_omni_extblogid" content="msstoextblogs2" />
+	<meta name="t_omni_blogname" content="tobiahmarks" />
+	<meta name="t_omni_market" content="US" />
+	<meta name="t_omni_audience" content="DEVELOPER" />
+	<script type="text/javascript" src="http://www.microsoft.com/feeds/omni_external_blogs.js"></script>
+	<noscript><a href='http://www.omniture.com' title='Web Analytics'><img src='http://mssto.112.2o7.net/b/ss/msstoextblogsnojs/1/H.20.2--NS/0'  height='1' width='1' border='0' alt='' /></a></noscript>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyfifteen' ); ?></a>
+
+	<div id="sidebar" class="sidebar">
+		<header id="masthead" class="site-header" role="banner">
+			<div class="site-branding">
+				<p><img src="http://www.tobiahz.com/images/TobiahZarlezHeadshot248.jpg" height="248" width="248" alt="Tobiah Zarlez Headshot"></p>
+				<?php
+					if ( is_front_page() && is_home() ) : ?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php else : ?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php endif;
+
+					$description = get_bloginfo( 'description', 'display' );
+					if ( $description || is_customize_preview() ) : ?>
+						<p class="site-description"><?php echo $description; ?></p>
+					<?php endif;
+				?>
+				<button class="secondary-toggle"><?php _e( 'Menu and widgets', 'twentyfifteen' ); ?></button>
+			</div><!-- .site-branding -->
+		</header><!-- .site-header -->
+
+		<?php get_sidebar(); ?>
+	</div><!-- .sidebar -->
+
+	<div id="content" class="site-content">
